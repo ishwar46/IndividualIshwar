6480adc192a2caf408ad27e42e4cb4d3a135681b
ishwar46 <ishwarura@gmail.com>
2022-12-28 22:14:35 +0545

    Bottom Nav Bar Added

--- news_app/lib/pages/homepage.dart
+++ news_app/lib/pages/homepage.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({super.key});
@@ -21,7 +20,49 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return DefaultTabController(
+      length: 6,
+      child: Scaffold(
+        bottomNavigationBar: Container(
+          color: Colors.black,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+            child: GNav(
+              backgroundColor: Colors.black,
+              rippleColor: Colors.grey[300]!,
+              hoverColor: Colors.grey[100]!,
+              gap: 8,
+              activeColor: Colors.purple,
+              iconSize: 20,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              duration: const Duration(milliseconds: 800),
+              tabBackgroundColor: Colors.grey[100]!,
+              color: Colors.white,
+              tabs: const [
+                GButton(
+                  icon: Icons.home,
+                  text: 'Home',
+                ),
+                GButton(
+                  icon: Icons.new_releases_rounded,
+                  text: 'Latest',
+                ),
+                GButton(
+                  icon: Icons.favorite,
+                  text: 'Favourites',
+                ),
+                GButton(
+                  icon: Icons.person,
+                  text: 'Profile',
+                ),
+              ],
+              selectedIndex: 0,
+              onTabChange: (index) {
+                print(index);
+              },
+            ),
+          ),
+        ),
         drawer: Drawer(
           child: ListView(
             children: [
@@ -57,19 +98,44 @@ class _HomePageState extends State<HomePage> {
         body: SafeArea(
           child: SingleChildScrollView(
             child: Column(
-              children: [
-                // Disover Text
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 20),
-                  child: Text(
-                    "Discover",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                //Navigation Bar
+                TabBar(
+                  isScrollable: true,
+                  physics: BouncingScrollPhysics(),
+                  unselectedLabelColor: Colors.black,
+                  labelColor: Colors.purple,
+                  indicatorColor: Colors.purple,
+                  tabs: [
+                    Tab(
+                      text: "All",
                     ),
-                  ),
+                    Tab(
+                      text: "Latest",
+                    ),
+                    Tab(
+                      text: "Popular",
+                    ),
+                    Tab(
+                      text: "Province",
+                    ),
+                    Tab(
+                      text: "Sports",
+                    ),
+                    Tab(
+                      text: "Politics",
+                    ),
+                  ],
                 ),
-                //Tab Bar
+                //TabBarView
+                // Expanded(
+                //   child: TabBarView(children: [
+                //     AllNews(),
+                //     ProvinceNews(),
+                //     PopularNews(),
+                //   ]),
+                // ),
 
                 //List tiles for news
 
@@ -77,6 +143,8 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
