61d560c1cbf6d2d9b6d4ce9167f674592d834ff9
ishwar46 <ishwarura@gmail.com>
2023-01-03 07:51:34 +0545

    homepage added

---
+++ news_app/lib/models/news_model.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+
+class NewsItem {
+  final String title;
+  final String imageUrl;
+  final String description;
+
+  NewsItem({
+    required this.title,
+    required this.imageUrl,
+    required this.description,
+  });
+}
+
+class NewsItem1 {
+  final String title;
+  final String imageUrl;
+  final String description;
+
+  NewsItem1({
+    required this.title,
+    required this.imageUrl,
+    required this.description,
+  });
+}
+
+final List<NewsItem> newsItems = [
+  NewsItem(
+      title: "Bipul Chettri in Nepal",
+      imageUrl:
+          'https://www.wikidata.org/wiki/Q18596652#/media/File:Bipul_Chhetri_.png',
+      description: 'Bipul Chettri & The Travelling Band at Club Platinum.'),
+  NewsItem(
+      title: "Bipul Chettri in Nepal",
+      imageUrl:
+          'https://www.wikidata.org/wiki/Q18596652#/media/File:Bipul_Chhetri_.png',
+      description: 'Bipul Chettri & The Travelling Band at Club Platinum.'),
+  NewsItem(
+      title: 'Bipul Chettri in Nepal',
+      imageUrl:
+          'https://www.wikidata.org/wiki/Q18596652#/media/File:Bipul_Chhetri_.png',
+      description: 'Bipul Chettri & The Travelling Band at Club Platinum.'),
+  NewsItem(
+      title: 'Bipul Chettri in Nepal',
+      imageUrl:
+          'https://www.wikidata.org/wiki/Q18596652#/media/File:Bipul_Chhetri_.png',
+      description: 'Bipul Chettri & The Travelling Band at Club Platinum.'),
+  NewsItem(
+      title: 'Bipul Chettri in Nepal',
+      imageUrl:
+          'https://www.wikidata.org/wiki/Q18596652#/media/File:Bipul_Chhetri_.png',
+      description: 'Bipul Chettri & The Travelling Band at Club Platinum.'),
+  NewsItem(
+      title: 'Bipul Chettri in Nepal',
+      imageUrl:
+          'https://www.wikidata.org/wiki/Q18596652#/media/File:Bipul_Chhetri_.png',
+      description: 'Bipul Chettri & The Travelling Band at Club Platinum.'),
+];
